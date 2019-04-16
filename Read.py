4a405bcd8791c246b21faf82d85740ba2c304305
Marlon Breaux <marlonbreaux24@gmail.com>
2019-04-16 16:42:01 -0500

    Update Read.py

--- Read.py
+++ Read.py
@@ -1,14 +1,14 @@
 f = open("readin.txt", "r+")  #Draws the file in 
 print(f.read())             #Prints the file
 def word_count(str):
-    counts = dict()
-    words = str.split()
+    counts = dict()         #Counting Words in a String
+    words = str.split()     #Spliting the words in that String 
 
     for word in words:
         if word in counts:
-            counts[word] += 1
+            counts[word] += 1     #For each word repeated, increment the number
         else:
-            counts[word] = 1
+            counts[word] = 1      #For each word, number it 1
 
-    return counts
+    return counts                 #Return the results
 print (word_count ('Three Tomatoes were walking down the street. Mama Tomato, Papa Tomato and Baby Tomato. Baby Tomato was lacking behind. Papa Tomato went back to Baby Tomato and squished him. And said "ketchup".'))
